5d3cd946fdc26c1d333ba599951b6983de22ef5b
ThibautDeduit <149255011+ThibautDeduit@users.noreply.github.com>
2024-01-31 23:26:33 +0100

    Add scores, refactor

--- script.js
+++ script.js
@@ -1,74 +1,106 @@
-const weapons = ["rock", "paper", "scissors"]
+let refereeMessage = "Here comes a new challenger!"
+const weapons = ["Rock", "Paper", "Scissors"]
 
-function getPlayerChoice() {
-	let playerChoice
-	while (!weapons.includes(playerChoice)) {
-		playerChoice = prompt("Do you choose rock, paper, or scissors?")
-			.toLowerCase()
-	}
-	return playerChoice
+let playerScore = 0
+let cpuScore = 0
+let scoreMessage = `Score: ${playerScore} - ${cpuScore}`
+
+function promptPlayer() {
+	let playerInput =  prompt(
+		`${refereeMessage}\n
+		${scoreMessage}\n
+		Do you choose Rock, Paper, or Scissors?`
+	)
+	let capitalizedPlayerInput = playerInput.at(0).toUpperCase()
+		+ playerInput.slice(1).toLowerCase()
+	return capitalizedPlayerInput
 }
 
-function getCpuChoice() {
+function getPlayerWeaponChoice() {
+	let playerWeaponChoice
+	while (!weapons.includes(playerWeaponChoice)) {
+		playerWeaponChoice = promptPlayer()
+	}
+	return playerWeaponChoice
+}
+
+function getCpuWeaponChoice() {
 	return weapons[parseInt(Math.random()*3)]
 }
 
 function playRound(weapon1, weapon2) {
 	switch (weapon1) {
-		case "rock":
+		case "Rock":
 			switch (weapon2) {
-				case "rock":
-					console.log(`It's a draw: ${weapon1} against ${weapon2}`)
+				case "Rock":
+					drawRound(weapon1, weapon2)
 					break
-				case "paper":
-					console.log(`You lose... ${weapon2} beats ${weapon1}`)
+				case "Paper":
+					loseRound(weapon1, weapon2)
 					break
-				case "scissors":
-					console.log(`You win! ${weapon1} beats ${weapon2}`)
+				case "Scissors":
+					winRound(weapon1, weapon2)
 					break
 				default:
-					console.log("Something's wrong with weapon2")
+					refereeMessage = "Something's wrong with weapon2"
 					break
 			}
 			break
-		case "paper":
+		case "Paper":
 			switch (weapon2) {
-				case "rock":
-					console.log(`You win! ${weapon1} beats ${weapon2}`)
+				case "Rock":
+					winRound(weapon1, weapon2)
 					break
-				case "paper":
-					console.log(`It's a draw: ${weapon1} against ${weapon2}`)
+				case "Paper":
+					drawRound(weapon1, weapon2)
 					break
-				case "scissors":
-					console.log(`You lose... ${weapon2} beats ${weapon1}`)
+				case "Scissors":
+					loseRound(weapon1, weapon2)
 					break
 				default:
-					console.log("Something's wrong with weapon2")
+					refereeMessage = "Something's wrong with weapon2"
 					break
 			}
 			break
-		case "scissors":
+		case "Scissors":
 			switch (weapon2) {
-				case "rock":
-					console.log(`You lose... ${weapon2} beats ${weapon1}`)
+				case "Rock":
+					loseRound(weapon1, weapon2)
 					break
-				case "paper":
-					console.log(`You win! ${weapon1} beats ${weapon2}`)
+				case "Paper":
+					winRound(weapon1, weapon2)
 					break
-				case "scissors":
-					console.log(`It's a draw: ${weapon1} against ${weapon2}`)
+				case "Scissors":
+					drawRound(weapon1, weapon2)
 					break
 				default:
-					console.log("Something's wrong with weapon2")
+					refereeMessage = "Something's wrong with weapon2"
 					break
 			}
 			break
 		default:
-			console.log("Something's wrong with weapon1")
+			refereeMessage = "Something's wrong with weapon1"
 			break
 	}
+	updateScore()
+}
+
+function winRound(weapon1, weapon2) {
+	refereeMessage = `You win! ${weapon1} beats ${weapon2}`
+	playerScore++
+}
+function loseRound(weapon1, weapon2) {
+	refereeMessage = `You lose... ${weapon2} beats ${weapon1}`
+	cpuScore++
+}
+function drawRound(weapon1, weapon2) {
+	refereeMessage = `It's a draw: ${weapon1} against ${weapon2}`
+}
+
+function updateScore() {
+	scoreMessage = `Score: ${playerScore} - ${cpuScore}`
 }
 
 while (true) {
-	playRound(getPlayerChoice(),getCpuChoice())
+	playRound(getPlayerWeaponChoice(), getCpuWeaponChoice())
 }
