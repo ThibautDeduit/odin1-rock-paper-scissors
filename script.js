812019e94c132d2ac688c55e6a638d979375ff82
ThibautDeduit <149255011+ThibautDeduit@users.noreply.github.com>
2024-01-31 21:49:42 +0100

    Add game rule, game loop

--- script.js
+++ script.js
@@ -12,5 +12,63 @@ function getPlayerChoice() {
 function getCpuChoice() {
 	return weapons[parseInt(Math.random()*3)]
 }
-console.log("The player chooses: " + getPlayerChoice())
-console.log("The computer chooses: " + getCpuChoice())
+
+function playRound(weapon1, weapon2) {
+	switch (weapon1) {
+		case "rock":
+			switch (weapon2) {
+				case "rock":
+					console.log(`It's a draw: ${weapon1} against ${weapon2}`)
+					break
+				case "paper":
+					console.log(`You lose... ${weapon2} beats ${weapon1}`)
+					break
+				case "scissors":
+					console.log(`You win! ${weapon1} beats ${weapon2}`)
+					break
+				default:
+					console.log("Something's wrong with weapon2")
+					break
+			}
+			break
+		case "paper":
+			switch (weapon2) {
+				case "rock":
+					console.log(`You win! ${weapon1} beats ${weapon2}`)
+					break
+				case "paper":
+					console.log(`It's a draw: ${weapon1} against ${weapon2}`)
+					break
+				case "scissors":
+					console.log(`You lose... ${weapon2} beats ${weapon1}`)
+					break
+				default:
+					console.log("Something's wrong with weapon2")
+					break
+			}
+			break
+		case "scissors":
+			switch (weapon2) {
+				case "rock":
+					console.log(`You lose... ${weapon2} beats ${weapon1}`)
+					break
+				case "paper":
+					console.log(`You win! ${weapon1} beats ${weapon2}`)
+					break
+				case "scissors":
+					console.log(`It's a draw: ${weapon1} against ${weapon2}`)
+					break
+				default:
+					console.log("Something's wrong with weapon2")
+					break
+			}
+			break
+		default:
+			console.log("Something's wrong with weapon1")
+			break
+	}
+}
+
+while (true) {
+	playRound(getPlayerChoice(),getCpuChoice())
+}
